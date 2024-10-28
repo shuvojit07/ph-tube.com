f2ab37ab353117bd079348fc4b29f2e552e947d0
Shuvojit Tikader <shuvojittikader@gmail.com>
2024-10-28 22:19:32 +0600

    3rd commit

--- js/videos.js
+++ js/videos.js
@@ -43,7 +43,7 @@ const loadvideos = () =>{
     .catch((error) => console.log(error));   
 };
 const loadCategorieyVideos = (id) => {
-alert(id);
+
 fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
 .then((res)=> res.json())
 .then((data) => displayVideos(data.category))
